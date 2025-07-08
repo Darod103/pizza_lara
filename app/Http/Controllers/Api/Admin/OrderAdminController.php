6039f55f1337ea883698d0a80c16efe1c41fa661
Vladimir Fedorei <darod103@gmail.com>
2025-07-08 16:23:35 +1000

    add Spatie for permission

---
+++ app/Http/Controllers/Api/Admin/OrderAdminController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OrderAdminController extends Controller
+{
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
