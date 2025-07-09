888e7b13eaee12c0b3419799c8806d67fc1caad8
Darod103 <darod103@gmail.com>
2025-07-09 22:14:09 +1000

    add destroy

--- routes/admin.php
+++ routes/admin.php
@@ -4,12 +4,13 @@ use App\Http\Controllers\Api\Admin\OrderAdminController;
 use App\Http\Controllers\Api\Product\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => ['auth:api','role:admin']], function () {
-    Route::delete('admin/order/{order}', [OrderAdminController::class,'destroy']);
-    Route::get('admin/order/',[OrderAdminController::class,'index']);
-    Route::post('admin/products/',[ProductController::class,'store']);
-    Route::patch('admin/products/{product}',[ProductController::class,'update']);
-    Route::put('admin/products/{product}',[ProductController::class,'update']);
-    Route::put('admin/order/{order}', [OrderAdminController::class,'update']);
-    Route::patch('admin/order/{order}', [OrderAdminController::class,'update']);
+Route::group(['middleware' => ['auth:api', 'role:admin']], function () {
+    Route::delete('admin/order/{order}', [OrderAdminController::class, 'destroy']);
+    Route::delete('admin/products/{product}', [ProductController::class, 'destroy']);
+    Route::get('admin/order/', [OrderAdminController::class, 'index']);
+    Route::post('admin/products/', [ProductController::class, 'store']);
+    Route::patch('admin/products/{product}', [ProductController::class, 'update']);
+    Route::put('admin/products/{product}', [ProductController::class, 'update']);
+    Route::put('admin/order/{order}', [OrderAdminController::class, 'update']);
+    Route::patch('admin/order/{order}', [OrderAdminController::class, 'update']);
 });
