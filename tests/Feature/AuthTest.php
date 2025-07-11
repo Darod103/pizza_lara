6c3eb1bd41b9baf0bbd4f821807b64361da21b18
Vladimir Fedorei <darod103@gmail.com>
2025-07-11 16:58:57 +1000

    Добавил два теста

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature;
+
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_auth_success(): void
+    {
+        $user = User::factory()->create([
+                'password' => '123456',
+            ]
+        );
+        $response = $this->post('/api/auth/login', [
+            'email' => $user->email,
+            'password' => '123456',
+        ]);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'access_token',
+            'token_type',
+            'expires_in'
+        ]);
+    }
+
+    public function test_auth_failed(): void
+    {
+        $user = User::factory()->create([
+            'password' => '123456',
+        ]);
+        $response = $this->post('/api/auth/login', [
+            'email' => $user->email,
+            'password' => '12345',
+        ]);
+        $response->assertStatus(401);
+        $response->assertJsonStructure([
+            'error'
+        ]);
+    }
+}
---
+++ tests/Tests/Feature/RegistrationTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+
+    public function test_registration_success(): void
+    {
+        $response = $this->postJson('api/register', [
+            'name' => 'Test User',
+            'email' => 'test@test.ru',
+            'password' => '123456',
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'email' => 'test@test.ru'
+        ]);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'name',
+            'email',
+            'access_token',
+            'expires_in'
+        ]);
+    }
+
+    public function test_registration_fail(): void
+    {
+        User::create([
+            'name' => 'Test User',
+            'email' => 'test@test.ru',
+            'password' => '123456',
+        ]);
+        $response = $this->postJson('api/register', [
+            'name' => 'Test User',
+            'email' => 'test@test.ru',
+            'password' => '123456',
+        ]);
+        $response->assertStatus(422);
+        $this->assertDatabaseCount('users', 1);
+    }
+
+    public function test_registration_fail_with_wrong_password(): void
+    {
+        $response = $this->postJson('api/register', [
+            'name' => 'Test User',
+            'email' => 'test@ro',
+            'password' => '',
+        ]);
+
+        $response->assertStatus(422);
+    }
+}
